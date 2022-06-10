698ed2aae489e22acafa335958706905b9f77660
jadashepard <jnshepard12@gmail.com>
2022-06-10 10:06:42 -0400

    All card images same size

--- js/index.js
+++ js/index.js
@@ -6,4 +6,20 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
+}
+//Dropdown functions
+function dropdownFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
+}
